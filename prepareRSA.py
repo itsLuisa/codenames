8755741f02c3db3d9581ff1d6a422fa4f67161e6
Luisa Geiger <lu.geiger3011@gmail.com>
2021-08-25 13:54:17 +0200

    changed variable names, made it cleaner

--- prepareRSA.py
+++ prepareRSA.py
@@ -7,9 +7,10 @@ from itertools import combinations
 # extract example
 with open("codenamesexp.json", encoding="utf-8") as f:
     data = json.load(f)
-    clue = data["game 3"]["round 1"]["clue"]
-    gold_guesses = data["game 3"]["round 1"]["guesses"]
-    remaining_words = data["game 3"]["round 1"]["remaining words"]
+    #clue = data["game 3"]["round 1"]["clue"]
+    clue = data["game 3"]["round 5"]["clue"]
+    gold_guesses = data["game 3"]["round 5"]["guesses"]
+    remaining_words = data["game 3"]["round 5"]["remaining words"]
 
 # make guesses
 model = GloVe_Model()
@@ -19,32 +20,35 @@ rank = model.get_hierarchy(clue[0], remaining_words)
 combos = dict()
 for c in combinations(rank, clue[1]):
     words = tuple(i[0] for i in c)
-    #print(words)
     m = mean([i[1] for i in c])
-    #print(m)
     combos[words] = m
 
 # sort combos
 sorted_combos = sorted(combos.items(), key=lambda x: x[1])
-#print(sorted_combos, len(sorted_combos))
 
 # shorten the combo list
-shorted_combos = list()
+short_combos = list()
+i = 0
 for (w,d) in sorted_combos:
-    shorted_combos.append((w,d))
-    if "battleship" in w and "crusader" in w: # here I have to find a more general threshold that makes sense
+    if i == 50:
         break
-print(shorted_combos, len(shorted_combos))
+    short_combos.append((w,d))
+    #if "battleship" in w and "crusader" in w: # here I have to find a more general threshold that makes sense
+        #break
+    i += 1
+
+print(len(short_combos), short_combos)
 
 # find alternative clues in the traditional Kim et al way
 alternative_clues = list()
-for c in shorted_combos:
+google_words_l = google_words(model)
+for c in short_combos:
     target_words = list(c[0])
-    print(target_words)
+    print("target words:", target_words)
     bad_words = [i for i in remaining_words if i not in target_words]
     #print(bad_words)
-    new_clue = produce_clue(target_words, bad_words, google_words(model), model)
-    print(new_clue)
+    new_clue = produce_clue(target_words, bad_words, google_words_l, model)
+    print("alternative clue:", new_clue)
     alternative_clues.append(new_clue)
 
 # find alternative clues in a (hopefully) faster, vectorbased way
@@ -57,8 +61,4 @@ for c in shorted_combos:
 # assemble clue combos
 clues = [tuple(clue)] + alternative_clues
 print(clues)
-
-# create RSA board / meaning matrix
-
-
-# RSA
+print(short_combos)
