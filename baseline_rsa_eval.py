02169f49bbaa55ac75f24ed0c5240f79ca5ea03f
Luisa Geiger <lu.geiger3011@gmail.com>
2021-09-28 10:37:41 +0200

    added Somers method

--- baseline_rsa_eval.py
+++ baseline_rsa_eval.py
@@ -9,19 +9,27 @@ model = GloVe_Model()
 with open("codenamesexp.json", encoding="utf-8") as f:
     data = json.load(f)
 
-with open("rsa_eval.tsv", "w", encoding="utf-8") as g:
+with open("rsa_eval_somers_250.tsv", "w", encoding="utf-8") as g:
     first_line = "original clue\tbest literal guess\tbest pragmatic guess\thuman guess\n"
     g.writelines(first_line)
 
 for game in data:
     for round in data[game]:
+        unknown_word = False
         if round != "color distribution":
             remaining_words = data[game][round]["remaining words"]
             clue = data[game][round]["clue"]
             gold_guesses = data[game][round]["guesses"]
             if clue[0] not in model.embeddings:
                 continue
-            original_clue, best_lit_guess, best_prag_guess, human_guess = rsa_based_guess(clue, gold_guesses, remaining_words, alt_clues, model)
-            with open("rsa_eval.tsv", "a", encoding="utf-8") as h:
+            if clue[1] == "inf":
+                continue
+            for word in remaining_words:
+                if word not in model.embeddings:
+                    unknown_word = True
+            if unknown_word:
+                continue
+            original_clue, best_lit_guess, best_prag_guess, human_guess = rsa_based_guess(clue, gold_guesses, remaining_words, alt_clues, model, 2)
+            with open("rsa_eval_somers_250.tsv", "a", encoding="utf-8") as h:
                 line = str(original_clue) + "\t" + str(best_lit_guess) + "\t" + str(best_prag_guess) + "\t" + str(human_guess) + "\n"
                 h.writelines(line)
