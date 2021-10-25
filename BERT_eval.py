70d2d7918d07013efb9ed91a3274958f3a00f978
Luisa Geiger <lu.geiger3011@gmail.com>
2021-10-25 17:32:24 +0200

    back up

--- BERT_eval.py
+++ BERT_eval.py
@@ -7,8 +7,8 @@ model = GloVe_Model()
 with open("data/codenamesexp.json", encoding="utf-8") as f:
     data = json.load(f)
 
-with open("results/bert_eval.tsv", "w", encoding="utf-8") as g:
-    first_line = "original clue\tbest baseline guess\tbest bert guess\thuman guess\n"
+with open("results/bert_eval_exsent.tsv", "w", encoding="utf-8") as g:
+    first_line = "original clue\tbest baseline guess\tbest bert guess\thuman guess\tscore\tpoints\n"
     g.writelines(first_line)
 
 for game in data:
@@ -31,6 +31,21 @@ for game in data:
             model_guess = bert_based_guess(remaining_words, clue)
             #original_clue, best_lit_guess, best_prag_guess, human_guess = rsa_based_guess(clue, gold_guesses, remaining_words, alt_clues, model)
 
-            with open("results/bert_eval.tsv", "a", encoding="utf-8") as h:
-                line = str(clue) + "\t" + "baseline insert here" + "\t" + str(model_guess) + "\t" + str(gold_guesses) + "\n"
+            flag = True
+            i = -1
+            while flag:
+                i += 1
+                try:
+                    flag = model_guess[i] in gold_guesses
+                except IndexError:
+                    flag = False
+            score = i
+
+            points = 0
+            for g in model_guess:
+                if g in gold_guesses:
+                    points += 1
+
+            with open("results/bert_eval_exsent.tsv", "a", encoding="utf-8") as h:
+                line = str(clue) + "\t" + "baseline insert here" + "\t" + str(model_guess) + "\t" + str(gold_guesses) + "\t" + str(score) + "\t" + str(points) + "\n"
                 h.writelines(line)
